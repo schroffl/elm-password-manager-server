1465190e7e4bd1f4a3645b7baceaab6f250cf250
schroffl <schroffl@users.noreply.github.com>
2017-09-04 17:58:50 +0200

    Renamed meta fields

--- src/Entry.hs
+++ src/Entry.hs
@@ -6,7 +6,19 @@ data Entry = Entry
   } deriving (Show, Eq)
 
 data EntryMeta = EntryMeta
-  { isLocal :: Bool
-  , isModified :: Bool
-  , modifiedAt :: Int
+  { isNew :: Bool
+  , isSynced :: Bool
+  , lastModified :: Int
   } deriving (Show, Eq)
+
+entryIsNew :: Entry -> Bool
+entryIsNew = isNew . meta
+
+newEntry :: Entry
+newEntry = Entry 0 $ EntryMeta True False 104
+
+syncedEntry :: Entry
+syncedEntry = Entry 1 $ EntryMeta False True 100
+ 
+modifiedEntry :: Entry
+modifiedEntry = Entry 1 $ EntryMeta False False 374
