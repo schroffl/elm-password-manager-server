4221f09ed645f89c15142b11518343192c35cc9f
schroffl <schroffl@users.noreply.github.com>
2017-09-05 15:34:10 +0200

    Experimenting with Haskells type system

--- src/Diff.hs
+++ src/Diff.hs
@@ -4,73 +4,53 @@ import Data.List
 
 import Entry
 
--- LocalState refers to the database that is maintained by the client
--- whereas RemoteState refers to the database on the server
-type RemoteState = [Entry]
+type RemoteState = [RemoteEntry]
 
-data LocalState = LocalState
-  { added :: [Entry]
-  , modified :: [Entry]
-  } deriving (Show, Eq)
+data ClientState = ClientState
+  { added :: [AddedEntry]
+  , modified :: [ModifiedEntry]
+  } deriving (Show)
 
-data Winner
-  = RemoteWins Entry
-  | LocalWins Entry
-  deriving (Show, Eq)
+combineState :: RemoteState -> ClientState -> (RemoteState, [ClientInstruction])
+combineState rState cState =
+  let withNewEntries =
+        foldl
+          (\(list, instructions) entry ->
+             let (newState, instruction) = addEntry list entry
+             in (newState, instruction : instructions))
+          (rState, [])
+          (added cState)
+      withModifiedEntries =
+        foldl
+          (\(list, instructions) entry ->
+             let (newState, instruction) = updateEntry list entry
+             in (newState, instruction : instructions))
+          withNewEntries
+          (modified cState)
+  in withModifiedEntries
 
-diff :: RemoteState -> LocalState -> [Winner]
-diff rState lState =
-  let addedEntries = map LocalWins (added lState)
-      modifiedEntries =
-        map pickWinner $ map (findMatch rState) (modified lState)
-  in modifiedEntries ++ addedEntries
+addEntry :: RemoteState -> AddedEntry -> (RemoteState, ClientInstruction)
+addEntry rState entry =
+  let newEntry = makeRemoteEntry (generateId rState) entry
+  in (newEntry : rState, makeInstruction newEntry entry)
 
-applyDiff :: RemoteState -> [Winner] -> RemoteState
-applyDiff = foldl applyWinner
+updateEntry :: RemoteState -> ModifiedEntry -> (RemoteState, ClientInstruction)
+updateEntry rState entry =
+  let pair = matchPair rState entry
+      winner = pickWinner pair
+  in case winner of
+       ClientWins _ ->
+         let newEntry = makeRemoteEntry 0 entry
+             instruction = makeInstruction newEntry entry
+         in (replaceSameEntry rState newEntry, instruction)
+       RemoteWins rEntry -> (rState, makeInstruction rEntry entry)
 
-applyWinner :: RemoteState -> Winner -> RemoteState
-applyWinner rState winner =
-  case winner of
-    LocalWins entry ->
-      let syncedEntry =
-            entry {meta = (meta entry) {isNew = False, isSynced = True}}
-      in if entryIsNew entry
-           then syncedEntry {Entry.id = generateId rState} : rState
-           else updateEntry rState syncedEntry
-    RemoteWins _ -> rState
+replaceSameEntry :: RemoteState -> RemoteEntry -> RemoteState
+replaceSameEntry [] _ = []
+replaceSameEntry (x:xs) entry
+  | rId x == rId entry = entry : xs
+  | otherwise = x : replaceSameEntry xs entry
 
-updateEntry :: RemoteState -> Entry -> RemoteState
-updateEntry [] entry = []
-updateEntry (x:xs) entry
-  | isSame x entry = entry : xs
-  | otherwise = x : updateEntry xs entry
-
-findMatch :: RemoteState -> Entry -> (Entry, Maybe Entry)
-findMatch rState entry = (entry, find (isSame entry) rState)
-
-isSame :: Entry -> Entry -> Bool
-isSame e1 e2 =
-  if (isNew $ meta e1) || (isNew $ meta e2)
-    then False
-    else Entry.id e1 == Entry.id e2
-
-pickWinner :: (Entry, Maybe Entry) -> Winner
-pickWinner (localEntry, maybeRE) =
-  case maybeRE of
-    Nothing -> LocalWins localEntry
-    Just remoteEntry ->
-      let remoteTime = lastModified $ meta remoteEntry
-          localTime = lastModified $ meta localEntry
-      in if localTime > remoteTime
-           then LocalWins localEntry
-           else RemoteWins remoteEntry
-
-generateId :: RemoteState -> Int
-generateId = succ . maximum . map Entry.id
-
-testRS :: RemoteState
-testRS =
-  [Entry 1 (EntryMeta False True 200), Entry 0 (EntryMeta False True 100)]
-
-testLS :: LocalState
-testLS = LocalState [newEntry] [Entry 0 (EntryMeta False False 200)]
+generateId :: RemoteState -> Id
+generateId [] = 0
+generateId rState = succ $ maximum $ map rId rState
--- src/Entry.hs
+++ src/Entry.hs
@@ -1,24 +1,91 @@
 module Entry where
 
-data Entry = Entry
-  { id :: Int
-  , meta :: EntryMeta
-  } deriving (Show, Eq)
+import Data.List
 
-data EntryMeta = EntryMeta
-  { isNew :: Bool
-  , isSynced :: Bool
-  , lastModified :: Int
-  } deriving (Show, Eq)
+type Id = Int
 
-entryIsNew :: Entry -> Bool
-entryIsNew = isNew . meta
+type Timestamp = Int
 
-newEntry :: Entry
-newEntry = Entry 0 $ EntryMeta True False 104
+data Winner a
+  = RemoteWins RemoteEntry
+  | ClientWins a
+  deriving (Show)
 
-syncedEntry :: Entry
-syncedEntry = Entry 1 $ EntryMeta False True 100
- 
-modifiedEntry :: Entry
-modifiedEntry = Entry 1 $ EntryMeta False False 374
+type EntryPair a = (a, Maybe RemoteEntry)
+
+class ComparableEntry a where
+  matchPair :: [RemoteEntry] -> a -> EntryPair a
+  pickWinner :: (EntryPair a) -> Winner a
+
+class ConvertableEntry a where
+  makeRemoteEntry :: Id -> a -> RemoteEntry
+  makeInstruction :: RemoteEntry -> a -> ClientInstruction
+
+data RemoteEntry = RemoteEntry
+  { rId :: Id
+  , rTimestamp :: Timestamp
+  } deriving (Show, Read)
+
+-- AddedEntry denotes an Entry that has been added on the client
+-- and never been known by the server before
+data AddedEntry = AddedEntry
+  { aId :: Id
+  , aTimestamp :: Timestamp
+  } deriving (Show)
+
+instance ComparableEntry AddedEntry where
+  matchPair entries entry = (entry, Nothing)
+  pickWinner (entry, _) = ClientWins entry
+
+instance ConvertableEntry AddedEntry where
+  makeRemoteEntry id entry = RemoteEntry id (aTimestamp entry)
+  makeInstruction rEntry aEntry =
+    ClientInstruction (aId aEntry) (rId rEntry) (aTimestamp aEntry)
+
+-- ModifiedEntry denotes an Entry that has been changed on the client
+data ModifiedEntry = ModifiedEntry
+  { mId :: Id
+  , mTimestamp :: Timestamp
+  } deriving (Show)
+
+instance ComparableEntry ModifiedEntry where
+  matchPair entries entry = (entry, find ((== mId entry) . rId) entries)
+  pickWinner (clientEntry, maybeRE) =
+    case maybeRE of
+      Nothing -> ClientWins clientEntry
+      Just remoteEntry ->
+        if mTimestamp clientEntry > rTimestamp remoteEntry
+          then ClientWins clientEntry
+          else RemoteWins remoteEntry
+
+instance ConvertableEntry ModifiedEntry where
+  makeRemoteEntry _ entry = RemoteEntry (mId entry) (mTimestamp entry)
+  makeInstruction rEntry mEntry =
+    case pickWinner (mEntry, Just rEntry) of
+      ClientWins _ ->
+        ClientInstruction (mId mEntry) (mId mEntry) (mTimestamp mEntry)
+      RemoteWins _ ->
+        ClientInstruction (mId mEntry) (rId rEntry) (rTimestamp rEntry)
+
+-- SyncedEntry denotes an Entry that the client left unmodified 
+-- since the last request
+data SyncedEntry = SyncedEntry
+  { sId :: Id
+  , sTimestamp :: Timestamp
+  } deriving (Show)
+
+instance ComparableEntry SyncedEntry where
+  matchPair entries entry = (entry, find ((== sId entry) . rId) entries)
+  pickWinner (clientEntry, maybeRE) =
+    case maybeRE of
+      Nothing -> ClientWins clientEntry
+      Just remoteEntry ->
+        if sTimestamp clientEntry > rTimestamp remoteEntry
+          then ClientWins clientEntry
+          else RemoteWins remoteEntry
+
+data ClientInstruction = ClientInstruction
+  { target :: Id
+  , uId :: Id
+  , uTimestamp :: Timestamp
+  } deriving (Show)
